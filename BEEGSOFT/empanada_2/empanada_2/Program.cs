d3cc7872bb632d4393496d3b90a0f041986b737d
BeegSoft <beegsoft@hotmail.com>
2016-04-14 07:40:10 -0700

    Puse un hola

--- BEEGSOFT/empanada_2/empanada_2/Program.cs
+++ BEEGSOFT/empanada_2/empanada_2/Program.cs
@@ -19,6 +19,7 @@ namespace empanada_2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Inicio(ds));
+            //Algo
         }
     }
 }
